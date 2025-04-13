0472beccb8b5b6c6d5de5583ff5ab14391ef9c92
= <=>
2025-04-13 12:35:24 -0400

    Add app api to backend. Accept CORS in backend.

--- backend/expense_tool/settings.py
+++ backend/expense_tool/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -47,7 +49,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
+CORS_ALLOWED_ORIGINS = ["http://localhost:4200"]
 
 ROOT_URLCONF = 'expense_tool.urls'
 
